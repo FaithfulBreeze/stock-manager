9e52131d194ed6a640c223b9d0da3ffba8c4f42d
Luis Victor Silva Santos <97199882+FaithfulBreeze@users.noreply.github.com>
2024-05-15 12:08:28 -0300

    fixing small bug

--- src/public/javascript/deleteProduct.js
+++ src/public/javascript/deleteProduct.js
@@ -1,7 +1,7 @@
 const delete_button = document.querySelector('#delete_button')
 delete_button.addEventListener('click', (e) =>{
     e.preventDefault()
-    fetch(`https://52.67.163.103:3030/api/deleteProduct?id=${window.location.href.split('/')[3]}`, {
+    fetch(`https://52.67.163.103:3030/api/deleteProduct?id=${window.location.href.split('/')[1]}`, {
         method: 'DELETE'
     })
     .finally(window.location = '/')
